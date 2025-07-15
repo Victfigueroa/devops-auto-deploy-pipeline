824127f7b8fd7b3f166acf2d81560615dda6ad24
Víctor Figueroa <vict.figueroa.u@gmail.com>
2025-07-15 09:37:09 -0400

    Personalización completa de main con autoría de Víctor Figueroa

--- app.test.js
+++ app.test.js
@@ -1,10 +1,10 @@
 const getWelcomeMessage = require('./app.js');
 
-test('debería retornar el mensaje de bienvenida correcto', () => {
+test('debería retornar el mensaje de bienvenida correcto - MAIN', () => {
   expect(getWelcomeMessage()).toBe('¡Este sitio fue validado, fusionado y desplegado automáticamente!');
 });
 
-test('Autor del proyecto debe ser Víctor Figueroa', () => {
+test('Autor del proyecto debe ser Víctor Figueroa - MAIN', () => {
   const autor = 'Víctor Figueroa';
   expect(autor).toMatch(/Víctor Figueroa/);
 });
