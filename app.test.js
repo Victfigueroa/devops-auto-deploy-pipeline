d767257ea16e134388458670bb4f67ecd2b41b09
Víctor Figueroa <vict.figueroa.u@gmail.com>
2025-07-15 09:43:43 -0400

    Rama dev con personalización basada en main

--- app.test.js
+++ app.test.js
@@ -1,10 +1,10 @@
 const getWelcomeMessage = require('./app.js');
 
-test('debería retornar el mensaje de bienvenida correcto - MAIN', () => {
+test('debería retornar el mensaje de bienvenida correcto - DEV', () => {
   expect(getWelcomeMessage()).toBe('¡Este sitio fue validado, fusionado y desplegado automáticamente!');
 });
 
-test('Autor del proyecto debe ser Víctor Figueroa - MAIN', () => {
+test('Autor del proyecto debe ser Víctor Figueroa - DEV', () => {
   const autor = 'Víctor Figueroa';
   expect(autor).toMatch(/Víctor Figueroa/);
 });
